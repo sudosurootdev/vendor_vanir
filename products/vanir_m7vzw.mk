0a0a320b8f2ee20230fab1f6a42a1635d53f7efc
DHOMD <DHO@dho.im>
2015-03-10 18:50:28 -0400

    5.1 LMY47D
    Change-Id: I2c271b916e7e403bf3c98ed95dc848c809083d52

--- products/vanir_m7vzw.mk
+++ products/vanir_m7vzw.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/vanir/products/multi_m7-common.mk)
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LRX22G BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.0/LRX22G/304035.1:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LMY47D BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.0/LMY47D/304035.1:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
 
 PRODUCT_NAME := vanir_m7vzw
 PRODUCT_DEVICE := m7vzw
