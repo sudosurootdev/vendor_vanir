18f5c7094664d76260aebb1fd8e2cc93b42183fd
sudosurootdev <sudosurootdev@gmail.com>
2015-04-14 09:22:52 -0400

    "LMY47O"
    Change-Id: Ia873a7e7b021c4bdc2e6fed88a4f2d67f6886799
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

--- products/vanir_m7vzw.mk
+++ products/vanir_m7vzw.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/vanir/products/multi_m7-common.mk)
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LMY47I BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.1/LMY47I/304035.1:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LMY47O BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.1/LMY47O/304035.1:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
 
 PRODUCT_NAME := vanir_m7vzw
 PRODUCT_DEVICE := m7vzw
