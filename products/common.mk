c042d8e0ea6f3d2aac9f816d19a73233749bdfce
Adnan Begovic <adnan@cyngn.com>
2015-05-05 18:29:29 -0700

    Build Platform resource package.
    Change-Id: Id60f66b6db23989db1472a19bcb079b0083f7393
    
    Conflicts:
    	config/common.mk

--- products/common.mk
+++ products/common.mk
@@ -13,10 +13,7 @@ $(call inherit-product, vendor/vanir/proprietary/ringtones/VanirRingtones.mk)
 #ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
 ## CM Platform Library Resource Package
 PRODUCT_PACKAGES += \
-    org.cyanogenmod.platform-res
-
-## CM Platform Library
-PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform-res \
     org.cyanogenmod.platform \
     org.cyanogenmod.platform.xml
 #endif
