4721c141398c237028483ccf62a786735344c797
nuclearmistake <nuclearmistake@gmail.com>
2015-06-09 08:23:13 -0400

    Add tab ringtones to tv
    Change-Id: I1b9adf68badad30bc5edef00189a3b99f2be176d

--- products/common_tv.mk
+++ products/common_tv.mk
@@ -5,3 +5,6 @@ $(call inherit-product, vendor/vanir/products/common.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Helium.ogg
+
+# Audio Packages
+include frameworks/base/data/sounds/AudioPackage7alt.mk
