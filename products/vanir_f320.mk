9bfa749c8ed8b9074f794e7a6a20290d5f7c3911
sudosurootdev <sudosurootdev@gmail.com>
2015-06-21 17:07:29 -0400

    Remove D803 unless a Lollipop modem is released. Add f320 G2 variant
    Change-Id: I8b50cbe76c3e3a68a290044e6105775ec4b4a22a
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

---
+++ products/vanir_f320.mk
@@ -0,0 +1,21 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=vanir-f320
+
+# Inherit device configuration
+$(call inherit-product, device/lge/f320/f320.mk)
+
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/multi_g2-common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := f320
+PRODUCT_NAME := vanir_f320
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-F320
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_skt_kr/g2:5.1/LMY48B/F320S28d.1418740902:user/release-keys PRIVATE_BUILD_DESC="g2_skt_kr-user 5.1 LMY48B F320S28d.1418740902 release-keys"
