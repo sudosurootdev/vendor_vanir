937cca7040227a933fc61eb61fe47fd2753a4099
nuclearmistake <nuclearmistake@gmail.com>
2015-07-22 07:19:31 -0400

    Unify f400
    Change-Id: Idfbcaf94bd11eb2dfa37165ed285e4a8a60f035a

--- products/vanir_f400.mk
+++ products/vanir_f400.mk
@@ -3,18 +3,18 @@ DHO_VENDOR := vanir
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
-    ro.goo.rom=vanir-f400k
+    ro.goo.rom=vanir-f400
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/lge/f400k/full_f400k.mk)
+$(call inherit-product, device/lge/f400/full_f400.mk)
 
 # Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := f400k
-PRODUCT_NAME := vanir_f400k
+PRODUCT_DEVICE := f400
+PRODUCT_NAME := vanir_f400
