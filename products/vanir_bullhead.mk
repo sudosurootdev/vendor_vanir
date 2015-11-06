5ea973617f0ca7aa0b0147c07de369fa8dd5fbb7
nuclearmistake <nuclearmistake@gmail.com>
2015-11-06 10:17:28 -0500

    5X and 6P
    Change-Id: I7e1f9f8bfd6447ec5f59e787a4103420a53d7b81

---
+++ products/vanir_bullhead.mk
@@ -0,0 +1,24 @@
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit common phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := vanir_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5x
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0 MDB08M 2353240 release-key"
