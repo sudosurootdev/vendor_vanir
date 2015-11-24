e9eb539c0cdf4ffc4542065323db8cee3e869ad8
nuclearmistake <nuclearmistake@gmail.com>
2015-11-24 07:46:23 -0500

    Underp some properties
    Change-Id: I3b6583825b6e43d77d7f6af262b0ca37cfa142d8

--- products/common.mk
+++ products/common.mk
@@ -113,9 +113,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lge.proximity.delay=20
 endif
 
-# Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=MRA58K BUILD_ID=MRA58K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
-
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
     vendor/vanir/proprietary/common/xbin/sysro:system/xbin/sysro \
