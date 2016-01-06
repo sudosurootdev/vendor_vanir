1cf5cde3a8d709204efc8fa5ef969100d674b2ff
nuclearmistake <nuclearmistake@gmail.com>
2016-01-06 11:57:54 -0500

    vendorsetup.sh: add a gross workaround for cmsdk droiddoc
    Change-Id: I7f7dfe448d11301f4eb0851e2e5bcb89f371b246

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,4 +9,11 @@ for x in `COMBOS | sort -h`; do
   add_lunch_combo vanir_$x-userdebug
   add_lunch_combo vanir_$x-user
 done
-
+[ ! -e vendor/cm ] && mkdir -p vendor/cm
+[ ! -e vendor/cm/build ] && mkdir -p vendor/cm/build
+if [ ! -e vendor/cm ] || [ ! -e vendor/cm/build ]; then
+  mkdir -p vendor/cm/build/tools
+fi
+if [ ! -h vendor/cm/build/tools ] || [ ! -e "`readlink vendor/cm/build/tools`" ]; then
+  ln -sf `pwd`/vendor/vanir/build/tools vendor/cm/build/tools
+fi
