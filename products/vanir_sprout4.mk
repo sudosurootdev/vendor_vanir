a79d9b1030b624ef6d9b65552491dfaacbfa7a79
nuclearmistake <nuclearmistake@gmail.com>
2016-01-06 07:39:28 -0500

    Run fingerprint batcher script
    Change-Id: Ia0569a6525b3b87635efedb93f7fd6d9341f5f08

--- products/vanir_sprout4.mk
+++ products/vanir_sprout4.mk
@@ -1,9 +1,11 @@
+DHO_VENDOR := vanir
+# Boot Animation
+PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/480x854.zip:system/media/bootanimation.zip
+
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
 # Common vanir stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
 PRODUCT_NAME := vanir_sprout4
 PRODUCT_DEVICE :=sprout4
@@ -11,10 +13,5 @@ PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := A1
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="sprout4"
-DHO_VENDOR := vanir
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/480x854.zip:system/media/bootanimation.zip
-
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="sprout4"
--- products/vanir_sprout8.mk
+++ products/vanir_sprout8.mk
@@ -1,9 +1,11 @@
+DHO_VENDOR := vanir
+# Boot Animation
+PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/480x854.zip:system/media/bootanimation.zip
+
 $(call inherit-product, device/google/sprout8/sprout8.mk)
 
 # Common vanir stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
 
 PRODUCT_NAME := vanir_sprout8
 PRODUCT_DEVICE :=sprout8
@@ -11,11 +13,5 @@ PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := A1
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="sprout8"
-
-DHO_VENDOR := vanir
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/480x854.zip:system/media/bootanimation.zip
-
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="sprout8"
