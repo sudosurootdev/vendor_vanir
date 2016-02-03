93411cc4b82e30cfce08e29af71c862a52284623
DHOMD <DHO@dho.im>
2016-02-02 20:17:23 -0500

    Ressurrect Nicki
    But this time from CM, not quasi in house support...ish
    
    Change-Id: I6775205f7a92b401143427c0582c7deb2a08fba7

---
+++ products/vanir_nicki.mk
@@ -0,0 +1,30 @@
+# when this builds, it will probably mess your phone up.
+# you might want to wait to try this unless you have a giant pair of low-hanging huevos
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-nicki
+
+# Device
+$(call inherit-product, device/sony/nicki/nicki.mk)
+
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/480x854.zip:system/media/bootanimation.zip
+
+# Inherit common phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+# Needed for bootlogo
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+# Branding
+PRODUCT_DEVICE := nicki
+PRODUCT_NAME := vanir_nicki
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := nicki
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_CHARACTERISTICS := phone
+
+# Release name
+PRODUCT_RELEASE_NAME := Xperiam
