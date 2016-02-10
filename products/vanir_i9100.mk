0c7a483c975797cc229669e049281f5320c3df84
mr-tweaker <aniket.lamba@gmail.com>
2016-02-10 10:27:06 +0530

    Added support for i9100

---
+++ products/vanir_i9100.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Squisher Choosing
+DHO_VENDOR := vanir
+
+# Boot
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/480x800.zip:system/media/bootanimation.zip
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-i9100
+
+# Release name
+PRODUCT_RELEASE_NAME := GT-I9100
+
+# Inherit some common Vanir stuff.
+$(call inherit-product, vendor/vanir/config/common_phones.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/i9100/full_i9100.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := i9100
+PRODUCT_NAME := vanir_i9100
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-I9100
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.1.2/JZO54K/I9100XWMS2:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.1.2 JZO54K I9100XWMS2 release-keys"
