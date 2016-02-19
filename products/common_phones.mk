85b46d7df1c4324ce7bffe773c3d9b340a4cddf1
razorloves <razorloves@gmail.com>
2016-02-18 23:47:47 -0600

    Revert "cm: add selective based spn (2/2)"
    Appears to be unused in CM13.
    
    This reverts commit 2cfe6b260bf743cbf3091095807b373037581181.
    
    Change-Id: Ia9beee776fd86f516ed9834f92530355a189f326

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -5,10 +5,6 @@ $(call inherit-product, vendor/vanir/products/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Selective SPN list for operator number who has the problem. 
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
