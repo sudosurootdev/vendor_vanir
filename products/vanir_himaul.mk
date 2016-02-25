dfd6be808d4e4d559138845a72837b571d4d978e
DHOMD <DHO@dho.im>
2016-02-24 20:17:19 -0500

    3 flavors of A9s
    Change-Id: I6a19383c12edcfcb6ab70f830ffc03d244f2098f

---
+++ products/vanir_himaul.mk
@@ -0,0 +1,27 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-himaul
+
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit device configuration
+$(call inherit-product, device/htc/himaul/full_himaul.mk)
+
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := vanir_himaul
+BOARD_VENDOR := htc
+TARGET_VENDOR := htc
+PRODUCT_DEVICE := himaul
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="htc_himaulatt" \
+    PRODUCT_NAME="himaulatt_na_gen_unlock" \
+    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:6.0/MRA58K/669696.12:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.35.617.12 CL669696 release-keys"
