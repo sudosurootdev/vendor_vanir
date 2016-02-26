b3a0683c1a475e34ff760b4a7575ca274c392d07
DHOMD <DHO@dho.im>
2016-02-25 22:01:42 -0500

    Fixor the find7 + s
    Change-Id: I2916ddd761dce66955137571f6920bb4614c9154

--- products/vanir_find7.mk
+++ products/vanir_find7.mk
@@ -9,6 +9,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/oppo/find7/find7.mk)
 
@@ -20,6 +23,8 @@ PRODUCT_NAME := vanir_find7
 PRODUCT_DEVICE := find7
 PRODUCT_BRAND := OPPO
 PRODUCT_MANUFACTURER := OPPO
-PRODUCT_MODEL := Find 7
+PRODUCT_MODEL := Find7
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" TARGET_DEVICE="FIND7"
--- products/vanir_find7s.mk
+++ products/vanir_find7s.mk
@@ -2,12 +2,14 @@
 DHO_VENDOR := vanir
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true \
     ro.goo.rom=vanir-find7s
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/oppo/find7s/find7s.mk)
@@ -20,7 +22,11 @@ PRODUCT_NAME := vanir_find7s
 PRODUCT_DEVICE := find7s
 PRODUCT_BRAND := OPPO
 PRODUCT_MANUFACTURER := OPPO
-PRODUCT_MODEL := Find 7
+PRODUCT_MODEL := Find7
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" TARGET_DEVICE="FIND7"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys \
+    PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" \
+    TARGET_DEVICE="FIND7"
