5c118c1ca67a5df45cd9211513ea6e88e4bbe93e
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:20:37 -0700

    cm: Introduce statusbar feature xml.
    TICKET: CYNGNOS-2289
    Change-Id: I1906717f69c5123d2d3f12e860e729810073a01e

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -2,6 +2,7 @@
 PRODUCT_COPY_FILES += \
     vendor/vanir/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
+    vendor/vanir/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml
 
 # CM Platform Library
 PRODUCT_PACKAGES += \
