94871ff73ebd07e5fc3ac35b539b3b5a4299a4b6
DHOMD <DHO@dho.im>
2016-04-04 20:08:20 -0400

    Let there be Lux
    Change-Id: Ie52a674e9839d2bb7735f1fb83c21b1f1d72053b

---
+++ products/vanir_lux.mk
@@ -0,0 +1,24 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-lux
+
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/lux/full_lux.mk)
+
+# Inherit vanir phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lux
+PRODUCT_NAME := vanir_lux
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := lux
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto X Play"
