5a8eb3463d3eaf599d78dda253a3986ace01faa0
DHOMD <DHO@dho.im>
2016-05-13 20:05:58 -0400

    Tweak lettuce.mk
    Change-Id: Idf87b515cd9b8f34cd0b14cee4d118333fe097fe

--- products/vanir_lettuce.mk
+++ products/vanir_lettuce.mk
@@ -9,12 +9,19 @@ $(call inherit-product, device/yu/lettuce/full_lettuce.mk)
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := lettuce
 PRODUCT_NAME := vanir_lettuce
-PRODUCT_BRAND := YU
-PRODUCT_MANUFACTURER := YU
-PRODUCT_MODEL := YU5010
+BOARD_VENDOR := yu
+PRODUCT_DEVICE := lettuce
+
+TARGET_VENDOR_PRODUCT_NAME := YUPHORIA
+TARGET_VENDOR_DEVICE_NAME := YUPHORIA
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUPHORIA PRODUCT_NAME=YUPHORIA
+
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUPHORIA PRODUCT_NAME=YUPHORIA
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=YU/YUPHORIA/YUPHORIA:5.1.1/LMY49J/YOG4PAS8A8:user/release-keys PRIVATE_BUILD_DESC="YUPHORIA-user 5.1.1 LMY49J YOG4PAS8A8 release-keys"
+## Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=YU/YUPHORIA/YUPHORIA:5.1.1/LMY49J/YOG4PAS8A8:user/release-keys \
+    PRIVATE_BUILD_DESC="YUPHORIA-user 5.1.1 LMY49J YOG4PAS8A8 release-keys"
+endif
