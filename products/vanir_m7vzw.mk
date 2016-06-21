c11123bbc035b19a58da9c3d5c193d0238f24d6a
nuclearmistake <nuclearmistake@gmail.com>
2016-06-21 17:52:13 -0400

    fix lettuce
    Change-Id: Ie3b2c795bed6ef50d791df8a936af4b412a314c0

--- products/vanir_m7vzw.mk
+++ products/vanir_m7vzw.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/vanir/products/multi_m7-common.mk)
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_ID=LRX22G BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" BUILD_ID=LRX22G PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys" PRODUCT_NAME=HTCOneVZW
 
 PRODUCT_NAME := vanir_m7vzw
 PRODUCT_DEVICE := m7vzw
