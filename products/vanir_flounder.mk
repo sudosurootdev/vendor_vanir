4fac63439b7950ac76c3f22bb2039b43a92a6913
nuclearmistake <nuclearmistake@gmail.com>
2016-08-03 09:56:50 -0400

    Update fingerprints
    Change-Id: I7649029ec10342c2618064200396af7817a60048

--- products/vanir_flounder.mk
+++ products/vanir_flounder.mk
@@ -37,7 +37,7 @@ PRODUCT_PACKAGES += com.android.nfc_extras
 # CM Overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MOB30P/2960889:user/release-keys PRIVATE_BUILD_DESC="volantis-user 6.0.1 MOB30P 2960889 release-keys" BUILD_ID=MOB30P
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MOB30W/3031100:user/release-keys PRIVATE_BUILD_DESC="volantis-user 6.0.1 MOB30W 3031100 release-keys" BUILD_ID=MOB30W
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := vanir_flounder
--- products/vanir_himawl.mk
+++ products/vanir_himawl.mk
@@ -1,12 +1,10 @@
 #Squisher Choosing
 DHO_VENDOR := vanir
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.rom=vanir-himawl
+PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-himawl
 
 # Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
 $(call inherit-product, device/htc/himawl/full_himawl.mk)
@@ -19,8 +17,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-verizon
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := vanir_himawl
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_himawl" \
-    PRODUCT_NAME="HTCOneM9vzw" \
-    BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:6.0/MRA58K/669662.7:user/release-keys" \
-PRIVATE_BUILD_DESC="3.37.605.7 CL669662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="htc_himawl" PRODUCT_NAME="HTCOneM9vzw" BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:6.0/MRA58K/669662.7:user/release-keys" PRIVATE_BUILD_DESC="3.37.605.7 CL669662 release-keys"
