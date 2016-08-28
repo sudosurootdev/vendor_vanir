3e75c4c348c70afe7d9b3a06da66f1ebc410463d
DHOMD <DHO@dho.im>
2016-08-28 19:07:44 -0400

    Tuna DIAF
    Change-Id: I6a62db9fb9784a53151f7b2a46d1ac2681955849

--- tweaks/tuna/tweaks.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_COPY_FILES +=  \
-    vendor/vanir/tweaks/tuna/vanirtweakapply:system/xbin/vanirtweakapply \
-    vendor/vanir/tweaks/tuna/vanir:system/xbin/vanir \
-    vendor/vanir/tweaks/tuna/vanir.cfg:system/etc/vanir.cfg
