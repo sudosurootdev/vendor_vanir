36db23086da931b9df563fa736f12b8e3f581570
nuclearmistake <nuclearmistake@gmail.com>
2016-10-07 07:20:56 -0400

    Fingerprints
    Change-Id: I14933120c49279c4598c3baefa72bb23e0099fed

--- products/vanir_jfltexx.mk
+++ products/vanir_jfltexx.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 NO_DRM_BLOBS := true
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHOJ2:user/release-keys PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHOJ2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
 
 #Tagging
 PRODUCT_NAME := vanir_jfltexx
