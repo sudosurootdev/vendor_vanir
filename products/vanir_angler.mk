825a9c5af199ec3f640d770a5668d422ae8b4708
DHOMD <DHO@dho.im>
2016-11-15 15:13:36 -0500

    Fingerprinted
    Change-Id: Idb7f585f61d4f5de201c32736f5be644d4260c7c

--- products/vanir_angler.mk
+++ products/vanir_angler.mk
@@ -19,4 +19,4 @@ PRODUCT_MODEL := Nexus 6P
 
 TARGET_VENDOR := huawei
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=angler BUILD_FINGERPRINT=google/angler/angler:7.0/NBD90X/3254009:user/release-keys PRIVATE_BUILD_DESC="angler-user 7.0 NBD90X 3254009 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=angler BUILD_FINGERPRINT=google/angler/angler:7.0/NBD91K/3318877:user/release-keys PRIVATE_BUILD_DESC="angler-user 7.0 NBD91K 3318877 release-keys"
