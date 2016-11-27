acef729865e468673978e2c0a658e0d8da2ba096
DHOMD <DHO@dho.im>
2016-11-27 09:10:23 -0500

    Gracefully kill NFC
    Change-Id: I8513c7fec42ac4f9095634cb1ac5d4252120f3ac

--- config/nfc_enhanced.mk
+++ config/nfc_enhanced.mk
@@ -1,4 +1,1 @@
-# This device supports CM enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/vanir/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml \
-    vendor/vanir/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
+# You're too late to this show
