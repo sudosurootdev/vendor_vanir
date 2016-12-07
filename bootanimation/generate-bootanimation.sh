b44f1637f04bd72eadb25110c713df1087fb4637
nuclearmistake <nuclearmistake@gmail.com>
2016-12-07 08:09:54 -0500

    Cleaning up boot animation generation
    Change-Id: I16844d729fd6546a88fd8d886956f3267ca74c29

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
-
+#!/bin/bash -ex
+HERE="$(dirname $0)"
 WIDTH="$1"
 HEIGHT="$2"
 HALF_RES="$3"
@@ -18,17 +18,14 @@ else
 fi
 
 RESOLUTION=""$IMAGESIZE"x"$IMAGESIZE""
-
-for x in `tar tfj vendor/vanir/bootanimation/bootanimation.zip --exclude '*/*'`; do
+for x in `tar tf $HERE/bootanimation.tar --exclude '*/*'`; do
     mkdir -p $OUT/bootanimation/$x
 done
-tar xfp "vendor/vanir/bootanimation/bootanimation.tar" --to-command "convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
+tar xfp "$HERE/bootanimation.tar" --to-command "convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
 
 # Create desc.txt
 echo "$WIDTH" "$HEIGHT" 30 > "$OUT/bootanimation/desc.txt"
-cat "vendor/vanir/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
-
-# Create bootanimation.zip
+cat "$HERE/desc.txt" >> "$OUT/bootanimation/desc.txt" # Create bootanimation.zip
 cd "$OUT/bootanimation"
 
 zip -qr0 "$OUT/bootanimation.zip" .
