f15c25c145aaf6066ad5be9513ff1dfffd136e77
Derek <Derekh82@gmail.com>
2016-12-11 13:11:27 -0500

    WTF did onyx go?

---
+++ products/vanir_onyx.mk
@@ -0,0 +1,32 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=vanir-onyx
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit vanir phone stuff
+$(call inherit-product, vendor/commotio/products/common_phones.mk)
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# Inherit device configuration
+$(call inherit-product, device/oneplus/onyx/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := vanir_onyx
+PRODUCT_DEVICE := onyx
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := onyx
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := onyx
+TARGET_VENDOR_DEVICE_NAME := onyx
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=onyx PRODUCT_NAME=onyx
