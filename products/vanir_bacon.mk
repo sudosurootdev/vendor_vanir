580ca3b271df724dc43f03f7fcf707c1ec07221e
Derek <Derekh82@gmail.com>
2016-12-11 11:07:07 -0500

    Fix build

--- products/vanir_bacon.mk
+++ products/vanir_bacon.mk
@@ -7,7 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += drm.service.enabled=true ro.goo.rom=vanir-bacon
 PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit common bacon files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
