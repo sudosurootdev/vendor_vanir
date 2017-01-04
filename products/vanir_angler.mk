68275d74bafbb2600fa3ea9d6e6ddf174c6b669d
nuclearmistake <nuclearmistake@gmail.com>
2017-01-04 06:53:17 -0500

    AOSP on hockeypucks works fine... anglers are not hockeypucks
    This reverts commit 129975562e72b453cc5cbf13abc32ffeef5d79f4.
    
    Change-Id: Ia3d0560ac203f8a864a19626b9da01c01cadf16b

---
+++ products/vanir_angler.mk
@@ -0,0 +1,23 @@
+# Boot Animation
+PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
+
+# Inherit common phone stuff
+NEXUS_CATEGORY := phones
+$(call inherit-product, vendor/vanir/products/common_nexus.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := vanir_angler
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=angler BUILD_FINGERPRINT=google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
