e71da951981472df44226c47a2a51a08109aea7c
nuclearmistake <nuclearmistake@gmail.com>
2017-01-04 06:46:50 -0500

    Reverse cm compatibility symlinking process
    Change-Id: I88a7a1ed0094e6321563095b7f26719cf66a9c57

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,11 +9,8 @@ for x in `COMBOS | sort -h`; do
   add_lunch_combo vanir_$x-userdebug
   add_lunch_combo vanir_$x-user
 done
-[ ! -e vendor/cm ] && mkdir -p vendor/cm
-[ ! -e vendor/cm/build ] && mkdir -p vendor/cm/build
-if [ ! -e vendor/cm ] || [ ! -e vendor/cm/build ]; then
-  mkdir -p vendor/cm/build/tools
-fi
-if [ ! -h vendor/cm/build/tools ] || [ ! -e "`readlink vendor/cm/build/tools`" ]; then
-  ln -sf `pwd`/vendor/vanir/build/tools vendor/cm/build/tools
+if [ -h vendor/cm/build/tools ]; then
+  rm -f vendor/cm/build/tools
+  rmdir vendor/cm/build
+  rmdir vendor/cm
 fi
