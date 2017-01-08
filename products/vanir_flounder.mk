2803f8f5a8a67b28adfb2d51edf847d0da0c3a0c
DHOMD <DHO@dho.im>
2017-01-08 11:16:57 -0500

    Regen Flounder
    Change-Id: I445550b5bb661239c8ee221a2139bb454190a694
    Signed-off-by: DHOMD <DHO@dho.im>

--- products/vanir_flounder.mk
+++ products/vanir_flounder.mk
@@ -1,45 +1,51 @@
 #Squisher Choosing
 DHO_VENDOR := vanir
 
-KERNEL_SPAM := AOSP 3.10.0 Prebuilt
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cpp.duplication=false \
+    ro.goo.rom=vanir-flounder
 
-# Run these first or they will not stick
-PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-FLOUNDER
+# Boot animation
+TARGET_SCREEN_WIDTH := 1536
+TARGET_SCREEN_HEIGHT := 2048
 
-# Tablet Overlays no radios
-PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_no_radio
+# Make CM related stuffs
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Snap \
+    Gello
 
-# Boot Animation
-PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/832x520.zip:system/media/bootanimation.zip
-
-# Enhanced NFC
-$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
+# Inherit common vanir files.
+$(call inherit-product, vendor/vanir/products/common_tabs.mk)
+PRODUCT_PACKAGE_OVERLAYS += vendor/vanir/overlay/tab_no_radio 
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
-
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 
+# CM Overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
+
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# It's a Nexus
+PRODUCT_IS_A_NEXUS := true
 
 # Inline kernel building
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := flounder_defconfig
+TARGET_KERNEL_CONFIG := lineage_flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 TARGET_PREBUILT_KERNEL := false
-
-# Extra Packages
-PRODUCT_PACKAGES += com.android.nfc_extras
-
-# CM Overlays
-DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-cm
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26M/3562722:user/release-keys PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26M 3562722 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := vanir_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26M/3562722:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26M 3562722 release-keys"
