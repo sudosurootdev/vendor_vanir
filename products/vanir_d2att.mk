c887a41b5a4c6493646d5199a662476bdc775161
DHOMD <DHO@dho.im>
2017-01-18 19:34:49 -0500

    Regen d2 mk file from Commotio.... they are very fragile
    Change-Id: Ib56cc7d99f743b5c248d748634d5058aed42feb8

--- products/vanir_d2att.mk
+++ products/vanir_d2att.mk
@@ -5,12 +5,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-d2att
 
 # Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Inherit common vanir stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
-
-$(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 PRODUCT_NAME := vanir_d2att
--- products/vanir_d2vzw.mk
+++ products/vanir_d2vzw.mk
@@ -5,13 +5,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=vanir-d2vzw
 
 # Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
 # Inherit common vanir stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
-
-$(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
 # Unified device flag for Verizon permissions
 VZW_UNIFIED := true
