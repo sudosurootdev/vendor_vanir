eb26f5176c4f9c6b4cbcfc955a2138d8fe4319a8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-20 23:03:27 +0800

    cm: overlay: Settings: Enable gesture settings
    We'll be placing our custom touchscreen gesture settings
    into the AOSP gesture settings.
    
    Change-Id: If9d5dbf155415a01eaa96e21749517c1a8e135e7

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -5,10 +5,6 @@ $(call inherit-product, vendor/vanir/products/common.mk)
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# IMS Default Permission
-PRODUCT_COPY_FILES += \
-    vendor/vanir/config/permissions/qcom_ims.xml:system/etc/default-permissions/qcom_ims.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
