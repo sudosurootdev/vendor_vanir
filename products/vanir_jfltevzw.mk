1d684b05dd7f22d04493c7948596c1633df181ae
DHOMD <DHO@dho.im>
2017-02-26 11:16:56 -0500

    Fingerprinted
    Change-Id: I128b8767a379f3d1e97f8f4ff9f2b98ec34a967c

--- products/vanir_jfltevzw.mk
+++ products/vanir_jfltevzw.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 # Inherit common Verizon Wireless Perms and Lib
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:5.0.1/LRX22C/I545VRUGOC1:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 5.0.1 LRX22C I545VRUGOC1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:5.0.1/LRX22C/I545VRSGPL1:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 5.0.1 LRX22C I545VRSGPL1 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
