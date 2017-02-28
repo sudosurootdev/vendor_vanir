ddff3cdb08975b95df152b47b73a4fe989d2c0c1
DHOMD <DHO@dho.im>
2017-02-27 20:02:31 -0500

    M8 aptx
    Change-Id: I31ad42d43e7899b208b3d5acb8abd07dbf3816db

--- products/vanir_m8.mk
+++ products/vanir_m8.mk
@@ -15,6 +15,9 @@ $(call inherit-product, device/htc/m8/device.mk)
 # Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
+# AptX b/c we can
+$(call inherit-product, vendor/qcom/binaries/bluetooth/bluetooth-vendor.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m8
 PRODUCT_NAME := vanir_m8
