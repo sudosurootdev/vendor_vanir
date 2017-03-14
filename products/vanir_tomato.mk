cb29f9c89fd873d703c577d773be19d175a80061
DHOMD <DHO@dho.im>
2017-03-13 22:31:43 -0400

    Tomatoes
    Change-Id: I4913fda221b06677515e8f7ce42c1e2ce641c998

--- products/vanir_tomato.mk
+++ products/vanir_tomato.mk
@@ -1,25 +1,27 @@
 # VanirAOSP (to the power of Frey, to the hammer of Thor)
-
 PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-tomato
 
 # Boot Animation
-PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1080
 
-# Inherit common product files.
-NO_DRM_BLOBS := true
+# Veggies
+$(call inherit-product, device/yu/tomato/full_tomato.mk)
 
 # Inherit common phone stuff
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
-$(call inherit-product, device/yu/tomato/full_tomato.mk)
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-# Naming Tidbits
 PRODUCT_NAME := vanir_tomato
-PRODUCT_DEVICE := tomato
 BOARD_VENDOR := yu
+PRODUCT_DEVICE := tomato
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
+
 TARGET_VENDOR_PRODUCT_NAME := YUREKA
 TARGET_VENDOR_DEVICE_NAME := YUREKA
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUREKA PRODUCT_NAME=YUREKA
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=YU/YUREKA/YUREKA:5.1.1/LMY49J/YOG4PAS8A4:user/release-keys PRIVATE_BUILD_DESC="YUREKA-user 5.1.1 LMY49J YOG4PAS8A4 release-keys"
