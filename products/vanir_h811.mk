2d8e61bcaebf4b0cd458661352fa69d37d309ea1
DHOMD <DHO@dho.im>
2017-04-09 13:28:32 -0400

    Prob fix things that are not broken
    But this is proper
    
    Change-Id: I7d8e2a0acbf4154543f8193d3a03ef537227ddac

--- products/vanir_h811.mk
+++ products/vanir_h811.mk
@@ -6,6 +6,10 @@ PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-h811
 # Boot Animation
 PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
 
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/lge/h811/full_h811.mk)
 
@@ -13,7 +17,10 @@ $(call inherit-product, device/lge/h811/full_h811.mk)
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := h811
 PRODUCT_NAME := vanir_h811
+PRODUCT_DEVICE := h811
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H811
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g4" PRODUCT_NAME="g4__tmo_us" BUILD_FINGERPRINT="lge/p1__tmo_us/p1:6.0/MRA58K/152940055675e:user/release-keys" PRIVATE_BUILD_DESC="p1__tmo_us-user 6.0 MRA58K 152940055675e release-keys"
--- products/vanir_h815.mk
+++ products/vanir_h815.mk
@@ -6,6 +6,10 @@ PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-h815
 # Boot Animation
 PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
 
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit common vanir files.
 $(call inherit-product, vendor/vanir/products/common_phones.mk)
 
@@ -13,7 +17,10 @@ $(call inherit-product, vendor/vanir/products/common_phones.mk)
 $(call inherit-product, device/lge/h815/full_h815.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := h815
 PRODUCT_NAME := vanir_h815
+PRODUCT_DEVICE := h815
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H815
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g4" PRODUCT_NAME="g4_global_com" BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys" PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
