f2caf296d9daba37e18a03954daf61e74d61b2da
DHOMD <DHO@dho.im>
2017-04-09 13:31:06 -0400

    2 G5s anf a G6
    Change-Id: I4e0ed7266151d5f8a6d02f70784164163c34c4a4
    Signed-off-by: DHOMD <DHO@dho.im>

---
+++ products/vanir_h830.mk
@@ -0,0 +1,32 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-h830
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/h830/full_h830.mk)
+# Inherit vanir phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_NAME := vanir_h830
+PRODUCT_DEVICE := h830
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H830
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g5" \
+    PRODUCT_NAME="h1_tmo_us" \
+    BUILD_FINGERPRINT="lge/h1_tmo_us/h1:7.0/NRD90U/1631213003394:user/release-keys" \
+PRIVATE_BUILD_DESC="h1_tmo_us-user 7.0 NRD90U 1631213003394 release-keys"
---
+++ products/vanir_h850.mk
@@ -0,0 +1,32 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-h850
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2560
+
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/h850/full_h850.mk)
+# Inherit vanir phone stuff
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_NAME := vanir_h850
+PRODUCT_DEVICE := h850
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H850
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g5" \
+    PRODUCT_NAME="h1_global_com" \
+    BUILD_FINGERPRINT="lge/h1_global_com/h1:7.0/NRD90U/163041049db12:user/release-keys" \
+PRIVATE_BUILD_DESC="h1_global_com-user 7.0 NRD90U 163041049db12 release-keys"
