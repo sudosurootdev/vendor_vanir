e79a6458c20ab851d6469c7d2734e6bb5115d84e
nuclearmistake <nuclearmistake@gmail.com>
2017-05-02 07:38:45 -0400

    Update fingerprints
    Change-Id: I5c5fdb9dd9c76243db27240047d9ac29197043b6

--- products/vanir_flounder.mk
+++ products/vanir_flounder.mk
@@ -40,4 +40,4 @@ PRODUCT_NAME := vanir_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26T/3687331:user/release-keys PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26T 3687331 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F27B/3853226:user/release-keys PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F27B 3853226 release-keys"
