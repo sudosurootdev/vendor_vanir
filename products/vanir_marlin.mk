81604361f4d797e861e4cd0dda5d6addbee04184
nuclearmistake <nuclearmistake@gmail.com>
2017-08-08 04:59:16 -0400

    Update fingerprints
    Change-Id: Ia6e3e922028126ce2955de7e7ecf85500ceb6032

--- products/vanir_marlin.mk
+++ products/vanir_marlin.mk
@@ -20,4 +20,4 @@ PRODUCT_MODEL := Pixel XL
 PRODUCT_MANUFACTURER := Google
 
 # Device Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=marlin BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NJH47D/4045516:user/release-keys PRIVATE_BUILD_DESC="marlin-user 7.1.2 NJH47D 4045516 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=marlin BUILD_FINGERPRINT=google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys PRIVATE_BUILD_DESC="marlin-user 7.1.2 NJH47F 4146041 release-keys"
