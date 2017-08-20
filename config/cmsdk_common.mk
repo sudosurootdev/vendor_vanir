93aa83364c6326ddcb086a80b66dc63c1baf8abe
Michael Bestas <mkbestas@lineageos.org>
2017-08-20 16:07:57 +0300

    cm: Remove LiveLockScreenService
    Change-Id: I8148e8a6c49f940c23b89e4f5fe697808dd08e93

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES += \
     vendor/vanir/config/permissions/org.cyanogenmod.audio.xml:system/etc/permissions/org.cyanogenmod.audio.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
-    vendor/vanir/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
     vendor/vanir/config/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
--- products/common.mk
+++ products/common.mk
@@ -37,7 +37,6 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     Jelly \
     ExactCalculator \
-    LiveLockScreenService \
     Screencast \
     WeatherProvider \
     WallpaperPicker
